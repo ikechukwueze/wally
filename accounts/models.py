0eeedc02e7bbea9234c62a64de9e7c43bc5ee44b
Ikechukwu Eze <iamikechukwueze@gmail.com>
2022-01-25 09:52:00 +0100

    created useraccount model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserAccount(models.Model):
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    email = models.EmailField(max_length=30)
